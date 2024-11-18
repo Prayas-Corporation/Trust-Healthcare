6cedf6dad34b763144707aa6260968fe3c1c5549
rishabh <aaarentalllp@Rishabh-MacBook-Pro.local>
2024-11-18 09:40:27 +0530

    changes next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'encrypted-tbn0.gstatic.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'www.shutterstock.com',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
